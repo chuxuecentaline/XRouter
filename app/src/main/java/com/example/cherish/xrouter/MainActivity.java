fc6e08159dcc7aa9831194f0613ea30e48bc57eb
chuxuecentaline <zxl@hanyu365.com.cn>
2019-02-26 13:03:11 +0800

    组件化开发初步

---
+++ app/src/main/java/com/example/cherish/xrouter/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.cherish.xrouter;
+
+import android.content.Intent;
+import android.view.View;
+
+import com.example.common.base.BaseActivity;
+import com.example.login.LoginActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void setContentView() {
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void mainRequest() {
+
+    }
+
+    @Override
+    protected void onClick() {
+
+    }
+
+    public void login(View view) {
+        startActivity(new  Intent(this,LoginActivity.class));
+    }
+}
